39a29bc5be31e822c6b3cb222d137fb61c15741d
Emmanuel Moran <emmanuel.moran@uabc.edu.mx>
2018-10-04 18:24:54 -0700

    Added List components and modified the reducer

---
+++ src/components/ListItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection, Card } from './common';
+
+class ListItem extends Component {
+    render() {
+        return (
+            <CardSection>
+                <Text>
+                    {this.props.library.item.title}
+                </Text>
+            </CardSection>
+        );
+    }
+}
+
+export default ListItem;
